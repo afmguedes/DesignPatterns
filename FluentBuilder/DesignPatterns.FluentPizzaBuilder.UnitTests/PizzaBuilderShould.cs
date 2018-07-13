da24a88f0a9bca67330b2bc667c311d8021929bf
Andre Guedes <andre.guedes@asos.com>
2018-07-13 15:09:54 +0100

    Add FluentPizzaBuilder and first unit test

---
+++ FluentBuilder/DesignPatterns.FluentPizzaBuilder.UnitTests/PizzaBuilderShould.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DesignPatterns.FluentPizzaBuilder.UnitTests
+{
+    [TestFixture]
+    public class PizzaBuilderShould
+    {
+        [Test]
+        public void ReturnAPizza_WhenBakeIsCalled()
+        {
+            var pizzaBuilder = new PizzaBuilder();
+
+            var actualPizza = pizzaBuilder.Bake();
+
+            actualPizza.Should().BeOfType<Pizza>();
+        }
+    }
+
+    public class Pizza
+    {
+    }
+
+    public class PizzaBuilder
+    {
+        public Pizza Bake()
+        {
+            return new Pizza();
+        }
+    }
+}
