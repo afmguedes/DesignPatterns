1d6c739393e5c32b5a145a1e3564192faa1e0424
Andre Guedes <andre.guedes@asos.com>
2018-07-13 15:31:39 +0100

    Add few more unit tests

--- FluentBuilder/DesignPatterns.FluentPizzaBuilder.UnitTests/PizzaBuilderShould.cs
+++ FluentBuilder/DesignPatterns.FluentPizzaBuilder.UnitTests/PizzaBuilderShould.cs
@@ -15,17 +15,75 @@ namespace DesignPatterns.FluentPizzaBuilder.UnitTests
 
             actualPizza.Should().BeOfType<Pizza>();
         }
+
+        [Test]
+        public void ReturnAPizzaWithSize_WhenWithSizeIsCalled()
+        {
+            var expectedPizza = new Pizza {Size = Size.Large};
+            var pizzaBuilder = new PizzaBuilder();
+
+            var actualPizza = pizzaBuilder
+                .WithSize(Size.Large)
+                .Bake();
+
+            actualPizza.Should().BeEquivalentTo(expectedPizza);
+        }
+
+        [Test]
+        public void ReturnAPizzaWithSizeAndCrust_WhenWithSizeAndWithCrustCalled()
+        {
+            var expectedPizza = new Pizza { Size = Size.Large, Crust = CrustType.Stuffed };
+            var pizzaBuilder = new PizzaBuilder();
+
+            var actualPizza = pizzaBuilder
+                .WithSize(Size.Large)
+                .WithCrust(CrustType.Stuffed)
+                .Bake();
+
+            actualPizza.Should().BeEquivalentTo(expectedPizza);
+        }
     }
 
     public class Pizza
     {
+        public Size Size { get; set; }
+        public CrustType Crust { get; set; }
+    }
+
+    public enum CrustType
+    {
+        Classic,
+        Stuffed,
+        Thin
+    }
+
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large,
+        ExtraLarge
     }
 
     public class PizzaBuilder
     {
+        private Pizza pizza = new Pizza();
+
+        public PizzaBuilder WithSize(Size size)
+        {
+            pizza.Size = size;
+            return this;
+        }
+
+        public PizzaBuilder WithCrust(CrustType typeOfCrust)
+        {
+            pizza.Crust = typeOfCrust;
+            return this;
+        }
+
         public Pizza Bake()
         {
-            return new Pizza();
+            return pizza;
         }
     }
 }
