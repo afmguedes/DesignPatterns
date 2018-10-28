a6423d6a0532428949bf86faddacfc2abf31c336
André Guedes <andre.guedes@asos.com>
2018-10-28 19:43:34 +0000

    Add PizzaRestaurant console app

---
+++ ChainOfResponsibility/DesignPatterns.PizzaRestaurant/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DesignPatterns.CORPizzaRestaurant;
+
+namespace DesignPatterns.PizzaRestaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var headChef = new HeadChef("Thiago");
+            var kitchenManager = new KitchenManager("Jonathan");
+            var generalManager = new GeneralManager("Yuliya");
+            var restaurant = new CORPizzaRestaurant.PizzaRestaurant(headChef, kitchenManager, generalManager);
+
+            Console.WriteLine("Welcome to Buona Sera Restaurant Order Approval System\n\n");
+
+            Console.WriteLine("Current hierarchy:\n");
+            foreach (var type in restaurant.GetHierarchy())
+            {
+                Console.WriteLine(type.Name);
+            }
+
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Order Approvals:\n");
+            var ordersToApprove = new[] { 1000, 150, 250, 10, 5000, 450, 600, 75, 50 };
+
+            foreach (var approval in restaurant.ProcessOrders(ordersToApprove))
+            {
+                Console.WriteLine(approval);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
